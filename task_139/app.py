eb97c3fcff16eedb7396bd17255ca7ef5c6aec53
d <d>
2019-11-18 16:37:54 +0100

    added route /spoje
    can call buses or trams from the api

--- task_139/app.py
+++ task_139/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, json
+from flask import Flask, request, render_template, json, jsonify
 # //import requests
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from datetime import datetime
@@ -7,21 +7,10 @@ from datetime import datetime
 app = Flask(__name__)
 json = FlaskJSON(app)
 
-@json.encoder
-def custom_encoder(o):
-    pass
-
 
 @app.route('/')
 def index():
     return 'Server Workssssssssssss!'
-
-
-@app.route('/gettime')
-def gettime():
-    now = datetime.utcnow()
-    return json_response(time=now)
-    # return type(now)
 
 
 @app.route('/bus/<line>')
@@ -29,8 +18,61 @@ def show_bus(line):
     return render_template('bus-lines.html', line=line)
 
 ##########nova funckia
-# def funkcia():
-#     return xyz
+
+@app.route('/spoje', methods=['GET', 'POST'])
+def spoje():
+    if request.method == 'POST':
+        if request.is_json:
+            key = request.json.get("printme")
+            # if key == "busy":
+            #     return jsonify(data["busy"])
+            # if key == "elektricky":
+            #     return jsonify(data["elektricky"])
+            # #return "JSON received", 200
+            # #return jsonify(req)
+            return jsonify(data[key])
+        else:
+            return "not a JSON", 400
+
+
+data = {
+    "busy": [
+        {
+            "id" : "1",
+            "smer" : "sever",
+            "cislo" : "11",
+            "driver_id" : "125",
+            "type" : "bus"
+    },
+    {
+        "id" : "2",
+        "smer" : "juh",
+        "cislo" : "12",
+        "driver_id" : "521",
+        "type" : "bus"
+    }
+    ],
+    "elektricky": [
+        {
+            "id" : "3",
+            "smer" : "zapad",
+            "cislo" : "5",
+            "driver_id" : "64684",
+            "type" : "elektricka"
+        },
+        {
+            "id" : "4",
+            "smer" : "vychod",
+            "cislo" : "7",
+            "driver_id" : "8852",
+            "type" : "elektricka"
+        }
+    ]
+}
+
+#print(type(data))
+
+
 
 
 
